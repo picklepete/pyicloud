acd4a2f7a43b3d38cb97b8cebf8abec17fc906d9
Tor Arne Vestbø <torarnv@gmail.com>
2016-02-23 21:12:53 +0100

    Add centralized logging of iCloud API requests
    Allows easier debugging of failing API calls. We filter out iCloud
    password so that debug logs can be attached to bug reports, etc.
    
    Errors are raised as PyiCloudAPIResponseError with a reason and
    code property, in addition to being logged, which allows them to
    be handled by client code, or will at least give a clearer idea
    about the issue than e.g. opaque key errors when trying to access
    non existent properties of the JSON response.

--- pyicloud/__init__.py
+++ pyicloud/__init__.py
@@ -1,1 +1,4 @@
+import logging
 from pyicloud.base import PyiCloudService
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
--- pyicloud/exceptions.py
+++ pyicloud/exceptions.py
@@ -7,6 +7,17 @@ class PyiCloudNoDevicesException(PyiCloudException):
     pass
 
 
+class PyiCloudAPIResponseError(PyiCloudException):
+    def __init__(self, reason, code):
+        self.reason = reason
+        self.code = code
+        message = reason
+        if code:
+            message += " (%s)" % code
+
+        super(PyiCloudAPIResponseError, self).__init__(message)
+
+
 class PyiCloudFailedLoginException(PyiCloudException):
     pass
 
