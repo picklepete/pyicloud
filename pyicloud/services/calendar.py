fa75c4fd4999b0d6f3971d39b3be1f012b16671a
Bart274 <de_van_bos@msn.com>
2016-03-11 22:03:22 +0100

    Add timezone from tzlocal

--- pyicloud/services/calendar.py
+++ pyicloud/services/calendar.py
@@ -4,6 +4,7 @@ from calendar import monthrange
 import time
 
 import pytz
+from tzlocal import get_localzone
 
 
 class CalendarService(object):
@@ -43,6 +44,10 @@ class CalendarService(object):
             for (utcoffset, daylight, tzname), _ in timezone._tzinfos.items():
                 if utcoffset == local_offset and tzname == localtz:
                     local_names.append(name)
+
+        if local_names == []:
+            name = get_localzone().name
+            local_names.append(name)
         return local_names
 
     def get_system_tz(self):
