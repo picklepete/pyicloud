0e26d4e9474e0b84707667d39c8b228149bdbc8f
Quentin POLLET <polletquentin74@me.com>
2020-03-09 22:08:51 +0100

    Remove duplicate PyiCloudNoDevicesException

--- pyicloud/exceptions.py
+++ pyicloud/exceptions.py
@@ -30,10 +30,6 @@ class PyiCloud2SARequiredError(PyiCloudException):
         super(PyiCloud2SARequiredError, self).__init__(message)
 
 
-class PyiCloudNoDevicesException(Exception):
-    pass
-
-
 class NoStoredPasswordAvailable(PyiCloudException):
     pass
 
