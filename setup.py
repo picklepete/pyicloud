05b2b57fc778457aec572af92c204071193e5338
Quentin POLLET <polletquentin74@me.com>
2020-03-20 12:33:58 +0100

    Travis not supporting 3.3

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
