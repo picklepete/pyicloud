2af7789ec3c1eb0d98d07425e6c336dc6959ca39
Paulus Schoutsen <balloob@gmail.com>
2022-02-16 14:18:51 -0800

    Lint

--- pyicloud/services/drive.py
+++ pyicloud/services/drive.py
@@ -229,7 +229,7 @@ class DriveService:
     def __getitem__(self, key):
         return self.root[key]
 
-    def _raise_if_error(self, response):
+    def _raise_if_error(self, response):  # pylint: disable=no-self-use
         if not response.ok:
             api_error = PyiCloudAPIResponseException(
                 response.reason, response.status_code
